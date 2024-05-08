37375c43710a0a249626a592a90e00be3bbdc46b
herparis <hernanleonparis@gmail.com>
2024-05-08 16:20:01 -0300

    add react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
-const App = () => null
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+type RootStackParamList = {
+  ContactList: undefined;
+  ContactDetails: undefined;
+};
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator initialRouteName='ContactList'>
+        <RootStack.Screen name='ContactList' component={() => null} />
+        <RootStack.Screen name='ContactDetails' component={() => null} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  )
+}
 
 export default App
