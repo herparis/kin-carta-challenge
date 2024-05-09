04e534762240bffb2c4f4bc0ca5f64e9243cc162
herparis <hernanleonparis@gmail.com>
2024-05-09 13:20:35 -0300

    add new splash screen

--- android/app/src/main/java/com/kincartachallenge/MainActivity.kt
+++ android/app/src/main/java/com/kincartachallenge/MainActivity.kt
@@ -5,9 +5,11 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 class MainActivity : ReactActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this);
     super.onCreate(null)
   }
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import { Provider } from 'react-redux'
+
 import store from './store'
 import RootStack from './navigation'
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <NavigationContainer>
